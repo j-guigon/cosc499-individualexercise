c2b4bbc063a01a2c84a71f4f2214fe6350122e32
j-guigon <87800430+j-guigon@users.noreply.github.com>
2022-09-14 17:33:58 -0700

    added a method to sum the values of the array.

--- individualExercise/src/App.java
+++ individualExercise/src/App.java
@@ -1,4 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        int my_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+
+        
+    }
+
+    public int sumArray(int[] x){
+        int sum = 0;
+        for (int i: x)
+            sum += i;
+        return sum;
     }
 }

f48e63fa963b3c326c549bbfb442cb68c05e6784
j-guigon <87800430+j-guigon@users.noreply.github.com>
2022-09-14 18:03:15 -0700

    method has passed modified unit test.

--- individualExercise/src/App.java
+++ individualExercise/src/App.java
@@ -1,11 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int my_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+        int[] my_array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
 
-        
+        //Output Sum of the values in the array
+        System.out.println("Sum: " + sumArray(my_array));
     }
 
-    public int sumArray(int[] x){
+    public static int sumArray(int[] x){
         int sum = 0;
         for (int i: x)
             sum += i;

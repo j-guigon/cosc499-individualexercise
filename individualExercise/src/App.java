f0de30a7383d68e0c5839913f09392c815257490
j-guigon <87800430+j-guigon@users.noreply.github.com>
2022-09-15 20:15:30 -0700

    modified method and passed JUnit tests.

--- individualExercise/src/App.java
+++ individualExercise/src/App.java
@@ -1,18 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] my_array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+        int[] my_array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
 
         //Output Average of the values in the array
         System.out.println("Average: " + avgArray(my_array));
     }
 
-    public static int avgArray(int[] x){
-        int avg = 0;
-        
-        int i = 0;
-        while(i<x.length)
-            avg += i;
-        avg = avg/i;
+    public static double avgArray(int[] x){
+        //calculate sum of all array elements
+        double sum = 0;
+        for(int i=0; i < x.length ; i++)
+         sum = sum + x[i];
+        //calculate average value
+         double avg = sum / x.length;
 
         return avg;
     }
--- individualExercise/src/AppTest.java
+++ individualExercise/src/AppTest.java
@@ -1,8 +1,11 @@
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     @Test
-    public void testMain() {
-
+    public void testAvgArray() {
+        // var app = new App();
+        int[] x = {1, 5, 4, 3, 10};
+        assertEquals(4.6, App.avgArray(x), 0.1);
     }
 }
